66f0b9532f824464ac3f57533d9cfbb7cfc12cd3
overblessed <overblessed5@gmail.com>
2023-02-17 10:20:07 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,5 @@
  * Return: 0
  */
 int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+{puts("\"Programming is like building a multilingual puzzle");return (0);
 }

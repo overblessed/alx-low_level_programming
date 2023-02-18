7b340e1a4e49f283af1d7c27231f421c30f0738a
overblessed <overblessed5@gmail.com>
2023-02-17 21:08:24 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (lc = 'a'; lc <= 's'; lc++)
+	for (lc = 'a'; lc <= 'f'; lc++)
 		putchar(lc);
 
 	putchar('\n');

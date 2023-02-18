c47bb2babbfef5173bb5a08cffa5d89844bfd1e5
overblessed <overblessed5@gmail.com>
2023-02-17 20:15:39 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers from 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{	int num;
+
+	for (num = 0; < 10; num++)
+		printf("%d", num);
+
+	print("\n");
+
+	return (0);
+}
